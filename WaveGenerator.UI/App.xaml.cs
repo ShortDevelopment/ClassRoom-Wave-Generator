4089caf5fcdb876a4ceadb5c05bb391a3a4e388d
Lukas Kurz <vb.net@kurzweb.de>
2021-06-19 16:15:15 +0200

    WebView Test

--- WaveGenerator.UI/App.xaml.cs
+++ WaveGenerator.UI/App.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Toolkit.Win32.UI.XamlHost;
+using System;
+using System.Diagnostics;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
 
 namespace WaveGenerator.UI
 {
@@ -8,6 +12,18 @@ namespace WaveGenerator.UI
         {
             this.Initialize();
             this.InitializeComponent();
+        }
+
+        async void WebViewTest()
+        {
+            var x = new WebViewBrush();
+            x.SourceName = "test";
+            var wvp = new Windows.Web.UI.Interop.WebViewControlProcess();
+            var wv = await wvp.CreateWebViewControlAsync((long)Process.GetCurrentProcess().MainWindowHandle, new Rect(new Point(0, 0), new Size(10, 10)));
+            wv.IsVisible = false;
+            wv.Navigate(new Uri("https://bing.com/"));
+            //x.SetSource(wv);
+            Debugger.Break();
         }
 
     }

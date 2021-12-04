9f0627aec650c2eef38b648dad9f31a3212fc642
Lukas Kurz <vb.net@kurzweb.de>
2021-12-04 12:44:04 +0100

    Remove old project

--- WaveGenerator.UI/Utils.cs
+++
@@ -1,9 +0,0 @@
-﻿using Windows.UI.Xaml.Markup;
-
-namespace WaveGenerator.UI
-{
-    public class Utils
-    {
-        public static T ConvertXamlValue<T>(object value) => (T)XamlBindingHelper.ConvertValue(typeof(T), value);
-    }
-}

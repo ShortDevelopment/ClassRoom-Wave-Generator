bf24377a0f046aa47fa421af84c3043d177b6f91
Lukas Kurz <lukas@shortdev.de>
2022-02-05 19:56:11 +0100

    Added Sentry

--- WaveGenerator/Program.cs
+++ WaveGenerator/Program.cs
@@ -17,19 +17,23 @@
 
             global::WinRT.ComWrappersSupport.InitializeComWrappers();
 #if !DEBUG
-            try
+            using (Sentry.SentrySdk.Init((o) =>
+                {
+                    o.Dsn = "https://f632072a23ca4c8db332f5efbdc0376d@o646413.ingest.sentry.io/6188290";
+                    o.Debug = true;
+                    o.TracesSampleRate = 1.0;
+                }))
             {
 #endif
-            global::Microsoft.UI.Xaml.Application.Start((p) =>
-            {
-                var context = new global::Microsoft.UI.Dispatching.DispatcherQueueSynchronizationContext(global::Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread());
-                global::System.Threading.SynchronizationContext.SetSynchronizationContext(context);
-                new App();
-            });
-#if !DEBUG
+                global::Microsoft.UI.Xaml.Application.Start((p) =>
+                {
+                    var context = new global::Microsoft.UI.Dispatching.DispatcherQueueSynchronizationContext(global::Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread());
+                    global::System.Threading.SynchronizationContext.SetSynchronizationContext(context);
+                    new App();
+                });
             }
-            catch (global::System.Exception) { }
-#endif
+#if !DEBUG
         }
+#endif
     }
 }

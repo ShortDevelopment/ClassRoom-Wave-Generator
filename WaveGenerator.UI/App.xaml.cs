b62ebfd066c8d25bf7cbe324ec1f109fbd80ef03
Lukas Kurz <lukas@shortdev.de>
2022-09-12 10:34:40 +0200

    Error Handling

--- WaveGenerator.UI/App.xaml.cs
+++ WaveGenerator.UI/App.xaml.cs
@@ -1,9 +1,28 @@
-﻿using Windows.UI.Xaml;
+﻿using System.Diagnostics;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace WaveGenerator
 {
     sealed partial class App : Application
     {
-        public App() { }
+        public App()
+        {
+            UnhandledException += App_UnhandledException;
+        }
+
+        private void App_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+#if DEBUG
+            if (Debugger.IsAttached)
+                Debugger.Break();
+#endif
+            ContentDialog errorDialog = new();
+            errorDialog.Title = e.Exception.GetType().FullName;
+            errorDialog.Content = e.Exception.Message;
+            errorDialog.PrimaryButtonText = "Ok";
+            _ = errorDialog.ShowAsync();
+        }
     }
 }
